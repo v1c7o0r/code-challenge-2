c242270daf3beefb9ac7dddfc4e50fe711c141a9
victor <162163319+v1c7o0r@users.noreply.github.com>
2024-04-18 20:52:23 +0300

    Update challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -1,12 +1,23 @@
-//generate a helping function that helps to identify prime numbers 
-function checkingPrimeNumbers(pn) {
-    if (pn % 2=== 0||pn % 3 === 0||pn <= 1||pn % 2!=0 ||pn % 5!=0 ||pn %7!=0)
-    return false ;
-    if (pn ===2||pn ===3)
-    return true;
+function primeNumbers(num) {
+    //check if number is less or equal to 1
+    if (num <= 1) {
+        return false;//if num is less than or equal to 1,it's not prime
+    }
+    //iterate from 2 to the square root of num
+    for (let x = 2; x <= Math.sqrt(num); x++) {
+        // If num is divisible by any number within the range [2, sqrt(num)], it's not prime
+        if (num % x === 0) {
+            return false;
+        }
+
+    }
+     // If num passes all checks, it's prime
+    return true
+
 }
- function filterPtimeNum (array){
-    return array.filter(number=>checkingPrimeNumbers(number));
- }
- const newArray = [1,2,3,4,5,6,7,8,9];
- console.log(filterPtimeNum(newArray));
+function allPrimeNumbers(numbers) {
+    return numbers.filter(num => primeNumbers(num));
+}
+//test code
+const primeeNumbers = allPrimeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+console.log(primeeNumbers);
